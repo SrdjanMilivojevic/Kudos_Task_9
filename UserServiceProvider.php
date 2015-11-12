21352462b46268b5eecd9e43743646fff47a6274
srdjancoyote <coyotewise@gmail.com>
2015-11-12 19:31:48 +0100

    code_tide_up

--- UserServiceProvider.php
+++ UserServiceProvider.php
@@ -4,14 +4,14 @@ class UserServiceProvider implements Provider
 {
     public function register(Container $container)
     {
-        // $db = $container->get("db"); // I guess this should be: $container->get("db", ['mysql:host=localhost;dbname=srdjan', 'root', '']);
-        $hash = $container->get("hash");
-        $container->set("UserService", function () use ($hash) {
-            return new UserService($hash);
+        $db = $container->get("db"); // I guess this should be: $container->get("db", ['mysql:host=localhost;dbname=srdjan', 'root', '']);z
+        // $hash = $container->get("hash");
+        $container->set("UserService", function () use ($db) {
+            return new UserService($db);
         });
 
-        $container->set("UserApplicationService", function () use ($hash) {
-            return new UserApplicationService($hash);
+        $container->set("UserApplicationService", function () use ($db) {
+            return new UserApplicationService($db);
         });
     }
 }
